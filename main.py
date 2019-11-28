abbc266eae6f4de8f4f6066b8ab0bb7db087db25
Thais Luca <thais.luca95@gmail.com>
2019-11-28 20:46:05 -0300

    Adding new stuff to main

--- main.py
+++ main.py
@@ -13,38 +13,37 @@ gc.collect()
 g, vertex_with_vertex = gt.create_graph(dt.load_file(UNGRAPH_AMAZON_NETWORK))
 g = gt.remove_artificial_vertex(vertex_with_vertex, g)
 gt.save_graph(g)
-#g = gt.load_graph_from_file()
 
+g = gt.load_graph_from_file()
 
+print("Load graph")
 
-# print("Load graph")
+d = 0
 
-# d = 0
+vertices = g.vertices()
+if d == 1:
+	degrees = []
 
-# vertices = g.vertices()
-# if d == 1:
-# 	degrees = []
+	#Find most popular vertex by its degree
+	most_popular = None
+	max_degree = 0
+	sum_degrees = 0
 
-# 	#Find most popular vertex by its degree
-# 	most_popular = None
-# 	max_degree = 0
-# 	sum_degrees = 0
+	for v in vertices:
+		d = v.out_degree()
+		sum_degrees += d 
+		degrees.append(d)
+		if d > max_degree:
+			max_degree = d 
+			most_popular = v
 
-# 	for v in vertices:
-# 		d = v.out_degree()
-# 		sum_degrees += d 
-# 		degrees.append(d)
-# 		if d > max_degree:
-# 			max_degree = d 
-# 			most_popular = v
+	print("Most popular item is %d with degree %d" % (most_popular, max_degree))
+	print("Average degree is %d" % (sum_degrees/g.num_vertices()))
+	#plt.plot_distribution(g.num_vertices(), degrees, "Graus", 'degrees')
+	plt.plot_ccdf(g.num_vertices(), degrees, "Graus", 'degrees')
 
-# 	print("Most popular item is %d with degree %d" % (most_popular, max_degree))
-# 	print("Average degree is %d" % (sum_degrees/g.num_vertices()))
-# 	#plt.plot_distribution(g.num_vertices(), degrees, "Graus", 'degrees')
-# 	plt.plot_ccdf(g.num_vertices(), degrees, "Graus", 'degrees')
+c = global_clustering(g)
+print("Global Clustering: %f DP: %f " % (c[0], c[1]))
+print("\n")
 
-# c = global_clustering(g)
-# print("Global Clustering: %f DP: %f " % (c[0], c[1]))
-# print("\n")
-
-# gc.collect()
+gc.collect()
