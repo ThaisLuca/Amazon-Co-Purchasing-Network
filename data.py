10f770274fafb32793d27f0535bdb87313b9ba42
= <thais.almeida@radixeng.com.br>
2019-11-30 20:00:29 -0300

    Method for vertex properties

--- data.py
+++ data.py
@@ -1,5 +1,6 @@
 
 import os, sys
+import pandas as pd
 
 # Loads network file into a matrix
 def load_file(filename):
@@ -17,6 +18,30 @@ def load_file(filename):
 	return data
 
 
-
-
-#def load_metadata_file(filename):
+def load_metadata_file(filename):
+	print("Reading meta data file...")
+	data = {}
+	title=salesrank=categories=rating=None
+	with open(filename, encoding="utf8") as f:
+		for line in f:
+			line = line.split()
+			try:
+				for i in range(0, len(line)):
+					if(line[i].startswith("Id:")):
+						Id = line[i+1]
+						break
+					elif(line[i].startswith("title")):
+						title = ' '.join(line[i+1:len(line)])
+						break
+					elif(line[i].startswith("salesrank")):
+						salesrank = line[i+1]
+						break
+					elif(line[i].startswith("categories")):
+						categories = line[i+1]
+						break
+					elif(line[i].startswith("avg") and line[i+1].startswith("rating")):
+						rating = line[i+2]
+			except:
+				print("Couldn't read character in position ", i)
+			data[Id] = {'Title': title, 'Salesrank': salesrank, 'Categories': categories, 'Rating': rating}
+	return data
